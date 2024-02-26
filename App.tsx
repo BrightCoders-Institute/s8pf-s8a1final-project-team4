bb6f11a68c2064cda6347b0181c3685eec62e628
YahirCL <ycortes1@ucol.mx>
2024-02-26 16:59:46 -0600

    Home design
    Home screen and inputs design

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import HomeScreen from './SRC/HomeScreen'
+
+export default function App() {
+  return (
+    <HomeScreen />  // This is the only change from the original file
+  )
+}
