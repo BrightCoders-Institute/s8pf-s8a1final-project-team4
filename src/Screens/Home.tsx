e7750382e8131914caa21613322612df3fdbfe67
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-03-12 11:55:15 -0600

    Added Home screen but icons are missing

---
+++ src/Screens/Home.tsx
@@ -0,0 +1,189 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+export default function Home() {
+  // const hi = 1;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerImg}>Img</Text>
+        <Text style={styles.headerText}>Hola, Usuario</Text>
+        <TouchableOpacity style={styles.congifIconContainer}>
+          <Text style={styles.configIcon}>Icon</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.accountContainer}>
+        <View>
+          <Text style={styles.accountTitle}>Cuentas</Text>
+          <Text style={styles.accountNum}>● 4545</Text>
+        </View>
+        <Text style={styles.accountBalance}>$1990</Text>
+      </View>
+      <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <TouchableOpacity style={styles.optionTouchable}>
+          <View style={styles.optionView}>
+            <Text style={styles.optionIcon}>Icon</Text>
+          </View>
+          <Text style={styles.optionText}>Transferir</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.optionTouchable}>
+          <View style={styles.optionView}>
+            <Text style={styles.optionIcon}>Icon</Text>
+          </View>
+          <Text style={styles.optionText}>Historial</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.optionTouchable}>
+          <View style={styles.optionView}>
+            <Text style={styles.optionIcon}>Icon</Text>
+          </View>
+          <Text style={styles.optionText}>Retirar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardView}>
+        <View style={styles.card}>
+          <View style={{flexDirection: 'row', gap: 12}}>
+            <TouchableOpacity>
+              <Text style={styles.cardIcon}>Icon</Text>
+            </TouchableOpacity>
+            <Text style={styles.cardNumber}>**** **** **** 4545</Text>
+          </View>
+          <Text style={styles.visaIcon}>Visa</Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Mis Tarjetas</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F2F2F2',
+    flex: 1,
+    gap: 40,
+  },
+  header: {
+    backgroundColor: '#4A52FF',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+  },
+  headerImg: {
+    height: 70,
+    width: 70,
+    borderRadius: 100,
+    backgroundColor: 'white',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 21,
+    fontWeight: '600',
+  },
+  congifIconContainer: {
+    alignSelf: 'flex-start',
+  },
+  configIcon: {
+    height: 20,
+    color: 'white',
+  },
+  accountContainer: {
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+    borderRadius: 3,
+    flexDirection: 'row',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 100},
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 12,
+  },
+  accountTitle: {
+    color: '#686868',
+    textTransform: 'uppercase',
+    fontSize: 16,
+  },
+  accountNum: {
+    color: 'black',
+    fontSize: 19,
+    paddingVertical: 30,
+    fontWeight: '900',
+  },
+  accountBalance: {
+    color: 'black',
+    fontSize: 28,
+  },
+  optionTouchable: {
+    alignItems: 'center',
+  },
+  optionView: {
+    backgroundColor: '#4A52FF',
+    borderRadius: 100,
+    height: 80,
+    width: 80,
+  },
+  optionIcon: {
+    height: 20,
+  },
+  optionText: {
+    color: '#4A52FF',
+    fontSize: 16,
+  },
+  cardView: {
+    borderLeftColor: '#4A52FF',
+    borderLeftWidth: 3,
+    paddingVertical: 10,
+    paddingLeft: 8,
+    marginHorizontal: 25,
+  },
+  card: {
+    backgroundColor: '#00079A',
+    borderRadius: 2,
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 35,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 100},
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  cardIcon: {
+    height: 25,
+    color: 'white',
+  },
+  cardNumber: {
+    color: 'white',
+    fontSize: 20,
+  },
+  visaIcon: {
+    height: 20,
+    position: 'absolute',
+    right: 15,
+    top: 15,
+  },
+  button: {
+    backgroundColor: '#4A52FF',
+    padding: 18,
+    width: 200,
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 3,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 100},
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
