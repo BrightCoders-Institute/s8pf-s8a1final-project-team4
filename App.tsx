929946c3897a9bb4da5bfa61bb11ea8410e1f3e5
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-03-25 12:22:01 -0600

    Added context for getting user info when logged

--- App.tsx
+++ App.tsx
@@ -1,14 +1,52 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MyStack from './src/StackNavigation/MyStack';
+import {db} from './src/Firebase/firebaseconfig';
+import {doc, setDoc, collection, addDoc, getDoc} from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const UserContext = createContext({
+  handleUserActive: () => {},
+});
 
 function App() {
+  const [userInfo, setUserInfo] = useState({});
+  const [userActive, setUserActive] = useState(false);
+
+  function handleUserActive() {
+    setUserActive(!userActive);
+  }
+
+  useEffect(() => {
+    console.log('useEffect running');
+    console.log(userActive);
+    (async () => {
+      try {
+        const userUid = await AsyncStorage.getItem('userUID');
+        const userDocRef = doc(db, 'users', userUid);
+        const docSnapshot = await getDoc(userDocRef);
+        if (docSnapshot.exists()) {
+          const userData = docSnapshot.data();
+          console.log(userData);
+          setUserInfo(userData);
+        } else {
+          console.log('no funciona');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [userActive]);
+
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <UserContext.Provider value={{userInfo, handleUserActive}}>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 }
 
 export default App;
+export {UserContext};
--- src/Screens/LogInFingerprint.tsx
+++ src/Screens/LogInFingerprint.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Icon2 from 'react-native-vector-icons/Feather';
 import FormButton from '../Components/Button';
 import {useNavigation} from '@react-navigation/native';
+import {GoogleAuthProvider, signInWithCredential} from 'firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {doc, setDoc, collection, addDoc, getDoc} from 'firebase/firestore';
+import {auth, db} from '../Firebase/firebaseconfig';
+
+function getRandomCardNumber() {
+  const cardNum = [];
+  for (let i = 0; i < 16; i++) {
+    const randomNum = Math.floor(Math.random() * 10);
+    cardNum.push(randomNum);
+  }
+  return cardNum.join('');
+}
 
 export default function LogInFingerprint() {
   const navigation = useNavigation();
+
+  const handleLogInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {idToken} = await GoogleSignin.signIn();
+      const googleCredential = GoogleAuthProvider.credential(idToken);
+      const result = await signInWithCredential(auth, googleCredential);
+      const uid = result.user.uid;
+      await AsyncStorage.setItem('userUID', uid);
+      // Crear documento en Firestore en la colección "users"
+      const userDocRef = doc(db, 'users', uid);
+      const docSnap = await getDoc(userDocRef);
+      //// CHECK IF EXIST
+      console.log(docSnap.exists());
+      if (!docSnap.exists()) {
+        const userData = {
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        };
+        await setDoc(userDocRef, userData);
+        // Crear documento en la subcolección "cards" dentro de la colección "users"
+        const cardsCollectionRef = collection(db, `users/${uid}/cards`);
+        const cardData = {
+          number: getRandomCardNumber(),
+          saldo: 10000,
+          tipo: 'debito',
+        };
+        await addDoc(cardsCollectionRef, cardData);
+        const cardData2 = {
+          number: getRandomCardNumber(),
+          saldo: 5000,
+          tipo: 'credito',
+        };
+        await addDoc(cardsCollectionRef, cardData2);
+      }
+      navigation.navigate('Home');
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Ocurrio un error al registrarse');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -22,7 +78,10 @@ export default function LogInFingerprint() {
           }}
         />
         <View style={{gap: 25}}>
-          <FormButton text={'Log in with Google'} fn={() => {}} />
+          <FormButton
+            text={'Log in with Google'}
+            fn={() => handleLogInWithGoogle()}
+          />
           <View style={styles.textView}>
             <Text style={styles.text}>If you don't have an account,</Text>
             <TouchableOpacity
