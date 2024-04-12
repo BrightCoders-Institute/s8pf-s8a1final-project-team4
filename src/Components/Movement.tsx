eccab7c4a8b1c31e56d06633ad83de3605df96d1
RicardoRam781 <alexrp782@gmail.com>
2024-04-12 12:54:58 -0600

    add corrections in movement component

--- src/Components/Movement.tsx
+++ src/Components/Movement.tsx
@@ -1,53 +1,120 @@
 import React from 'react'
-import { StyleSheet, View, Text} from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 //por ahora este componente tiene errores al manejarlo como funcion, en futuras issues se puede seccionar para poder reutilizarlo 
-const Movement = (transferName?: string, transferType?: string, number?: string) => {
+type movimiento = {
+    descripcion: string
+    tipo: string
+    monto: number
+}
+export const Movement = ({ item }: any) => {
     return (
-        <View>
-            <View style={styles.main}>
-                <View style={styles.card}>
-                    <View style={{ width: 30, height: 30, backgroundColor: "#54AB5F", borderRadius: 20 }} />
+        <View style={styles.main}>
+            <View style={styles.card}>
+
+                <View style={{ width: 30, height: 30, backgroundColor: "#54AB5F", borderRadius: 20 }} />
+                <View>
                     <View>
                         <View>
-                            <View>
-                                <Text style={styles.transferName}>{transferName}</Text>
-                                <Text style={styles.transferType}>{transferType}</Text>
-                            </View>
-                            <Text style={styles.number}>${number}</Text>
+                            <Text style={styles.transferName}>{item.descripcion}</Text>
+                            <Text style={styles.transferType}>{item.tipo}</Text>
                         </View>
+                        <Text style={styles.number}>${item.monto}</Text>
                     </View>
-
                 </View>
             </View>
         </View>
     )
+
 }
-export default Movement
+
+
+export const MovementFilterByRecived = ({item}:any) => {
+    return (
+
+        <View style={styles.main}>
+            {
+                item.monto > 0 &&
+                <View style={styles.card}>
+
+            <View style={{ width: 30, height: 30, backgroundColor: "#54AB5F", borderRadius: 20 }} />
+            <View>
+                <View>
+                    <View>
+                        <Text style={styles.transferName}>{item.descripcion}</Text>
+                        <Text style={styles.transferType}>{item.tipo}</Text>
+                    </View>
+                    <Text style={styles.number}>${item.monto}</Text>
+                </View>
+            </View>
+        </View>
+            }
+
+        
+
+    </View>
+        
+    )
+}
+export const MovementFilterBySent = ({item}:any) => {
+    return (
+
+        <View style={styles.main}>
+            {
+                item.monto < 0 &&
+                <View style={styles.card}>
+
+            <View style={{ width: 30, height: 30, backgroundColor: "red", borderRadius: 20 }} />
+            <View>
+                <View>
+                    <View>
+                        <Text style={styles.transferName}>{item.descripcion}</Text>
+                        <Text style={styles.transferType}>{item.tipo}</Text>
+                    </View>
+                    <Text style={[styles.number, {color:"red"}]}>${item.monto}</Text>
+                </View>
+            </View>
+        </View>
+            }
+
+        
+
+    </View>
+        
+    )
+}
 const styles = StyleSheet.create({
     main: {
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
-    },
-    card: {
+        alignItems: "center",
+      },
+      card: {
         flexDirection: "row",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        width: "90%"
-    },
-    date: {
-        color: '#00079A', fontSize: 22, fontWeight: "bold"
-    },
-    transferName: {
+        width: "90%",
+        },
+      date: {
+        color: '#00079A', fontSize: 22, fontWeight: "bold",margin:10
+      },
+      transferName: {
         color: "black", fontSize: 20
-    }, transferType: {
+      }, transferType: {
         fontSize: 15
-    },
-    number: {
+      },
+      number: {
         color: "#54AB5F",
-        fontSize: 22
-    }
+        fontSize: 22,
+        alignSelf:"flex-start"
+      },
+      texts:{
+        display:"flex",
+        flexDirection:"row",
+         width:300,
+        justifyContent:"space-between"
+    
+      }
 
 
 })
