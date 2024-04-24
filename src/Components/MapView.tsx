cdd132085974d555609b89c88d3368debc3a1bb5
RicardoRam781 <alexrp782@gmail.com>
2024-04-23 18:23:31 -0600

    styling map

--- src/Components/MapView.tsx
+++ src/Components/MapView.tsx
@@ -1,10 +1,12 @@
 import React,{useEffect, useState} from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import GetLocation, { Location } from 'react-native-get-location'
+import Icon from 'react-native-vector-icons/AntDesign';
+import { useNavigation } from '@react-navigation/native';
 export default function Mapsview() {
   const [location,setLocation] = useState<Location | undefined>()
-
+  const navigation = useNavigation()
   const getCurrentLocation = async () =>{
     try{
       const currenlocation = await GetLocation.getCurrentPosition({
@@ -21,25 +23,43 @@ export default function Mapsview() {
 
     return (
         <View style={style.container}>
+          
             <MapView
                 provider={PROVIDER_GOOGLE}
                 style={style.map}
                 initialRegion={location}
                 customMapStyle={mapStyle}
             />
+            <TouchableOpacity style={style.btn} onPress={() => {
+              navigation.goBack()
+            } }>
+            <Icon name='close' size={45} color={"white"} style={style.btn}/>
+            </TouchableOpacity>
+           
+         
+            
         </View>
     )
 }
 const style = StyleSheet.create({
     container: {
         ...StyleSheet.absoluteFillObject,
-        height:400,
-        width:400,
+        height:"100%",
+        width:"100%",
         justifyContent:"flex-end",
         alignItems:"center",
     },
     map:{
         ...StyleSheet.absoluteFillObject
+    },
+    btn:{
+
+      position:"absolute",
+      bottom:"93%",
+      left:"83%",
+      backgroundColor: 'rgba(0,0,0,0.2)',
+      borderRadius:30
+
     }
 });
 const mapStyle = [
